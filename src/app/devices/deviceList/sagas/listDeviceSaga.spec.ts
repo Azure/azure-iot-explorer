1f43f42c07d6ac220dc08d30ba2c9efc84b9f292
andrew-buckley <andrewbuckley@outlook.com>
2019-08-16 13:53:54 -0700

    Adds device list saga tests.

---
+++ src/app/devices/deviceList/sagas/listDeviceSaga.spec.ts
@@ -0,0 +1,101 @@
+/***********************************************************
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License
+ **********************************************************/
+import 'jest';
+import { SagaIteratorClone, cloneableGenerator } from 'redux-saga/utils';
+import { select, call, put } from 'redux-saga/effects';
+import { listDevicesSaga } from './listDeviceSaga';
+import DeviceQuery from '../../../api/models/deviceQuery';
+import * as DevicesService from '../../../api/services/devicesService';
+import { listDevicesAction } from '../actions';
+import { getConnectionStringSelector } from '../../../login/selectors';
+import { DeviceIdentity } from '../../../api/models/deviceIdentity';
+import { addNotificationAction } from '../../../notifications/actions';
+import { ResourceKeys } from '../../../../localization/resourceKeys';
+import { NotificationType } from '../../../api/models/notification';
+
+describe('listDeviceSaga', () => {
+    let listDevicesSagaGenerator: SagaIteratorClone;
+
+    const connectionString = 'connection_string';
+    const deviceId = 'device_id';
+    const mockDevices: DeviceIdentity[] = [
+            {
+            authentication: null,
+            capabilities: null,
+            cloudToDeviceMessageCount: '',
+            deviceId,
+            etag: 'etag',
+            lastActivityTime: '',
+            status: 'Enabled',
+            statusReason: '',
+            statusUpdatedTime: ''
+        }
+    ];
+    const query: DeviceQuery = {
+        clauses: [],
+        continuationTokens: [],
+        currentPageIndex: 0,
+        deviceId
+    };
+
+    const mockFetchDevices = jest.spyOn(DevicesService, 'fetchDevices').mockImplementationOnce(parameters => {
+        return null;
+    });
+
+    beforeAll(() => {
+        listDevicesSagaGenerator = cloneableGenerator(listDevicesSaga)(listDevicesAction.started(query));
+    });
+
+    it('fetches the connection string', () => {
+        expect(listDevicesSagaGenerator.next()).toEqual({
+            done: false,
+            value: select(getConnectionStringSelector)
+        });
+    });
+
+    it('fetches the devices', () => {
+        expect(listDevicesSagaGenerator.next(connectionString)).toEqual({
+            done: false,
+            value: call(mockFetchDevices, {
+                connectionString,
+                query
+            })
+        });
+    });
+
+    it('puts the successful action', () => {
+        const success = listDevicesSagaGenerator.clone();
+
+        expect(success.next(mockDevices)).toEqual({
+            done: false,
+            value: put(listDevicesAction.done({params: query, result: mockDevices as any})) // tslint:disable-line:no-any
+        });
+
+        expect(success.next().done).toEqual(true);
+    });
+
+    it('fails on error', () => {
+        const failure = listDevicesSagaGenerator.clone();
+        const error = { code: -1 };
+        expect(failure.throw(error)).toEqual({
+            done: false,
+            value: put(addNotificationAction.started({
+                text: {
+                    translationKey: ResourceKeys.notifications.getDeviceListOnError,
+                    translationOptions: {
+                      error,
+                  },
+                },
+                type: NotificationType.error,
+            }))
+        });
+
+        expect(failure.next(error)).toEqual({
+            done: false,
+            value: put(listDevicesAction.failed({params: query, error}))
+        });
+        expect(failure.next().done).toEqual(true);
+    });
+});
