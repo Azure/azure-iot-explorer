bc14491eb622babfbf5bd0070c99dee153d37da8
Rick Kessler <rkessler@microsoft.com>
2019-12-20 19:12:16 -0800

    update breadcrumbs.

--- src/app/shared/components/breadcrumb.tsx
+++ src/app/shared/components/breadcrumb.tsx
@@ -15,6 +15,12 @@ export interface BreadCrumbProps {
     hubName: string;
 }
 
+export interface BreadcrumbEntry {
+    text: string;
+    key: string;
+    href?: string;
+}
+
 export default class BreadcrumbComponent extends React.Component<BreadCrumbProps & RouteComponentProps>{
     constructor(props: BreadCrumbProps & RouteComponentProps) {
         super(props);
@@ -33,76 +39,113 @@ export default class BreadcrumbComponent extends React.Component<BreadCrumbProps
         );
     }
 
-    private readonly getShortHubName = (hostName: string) => {
-        return hostName && hostName.replace(/\..*/, '');
-    }
-
-    private readonly getItems = (t: TranslationFunction) => {
+       private readonly getItems = (t: TranslationFunction) => {
         const { hubName, location } = this.props;
         const { pathname, search } = location;
-        const items: Array<{ text: string, key: string, href?: string }> = [
-            { text: t(ResourceKeys.breadcrumb.hub, {hubName: this.getShortHubName(hubName)}), key: 'Hub'},
-            { text: t(ResourceKeys.breadcrumb.devices), key: 'Devices', href: `#/${ROUTE_PARTS.DEVICES}` },
-        ];
+        const pathComponents = pathname.split('/').filter(s => s !== '');
 
-        const frags = pathname && pathname.replace(/(#\/)?devices\//, '').split('/').filter(frag => '' !== frag);
-
-        if (frags.length > 0) {
-            const deviceFrag = this.getDeviceFrag(search);
-            if (deviceFrag) {
-                // remove the /detail and put in the deviceId
-                frags.splice(0, 1);
-                items.push(deviceFrag);
+        const items: BreadcrumbEntry[] = [];
+        // tslint:disable-next-line:cyclomatic-complexity
+        pathComponents.forEach((pathComponent, index) => {
+            if (pathComponent.toLowerCase() === ROUTE_PARTS.RESOURCE.toLowerCase() && index < pathComponents.length) {
+                items.push({
+                    key: 'Hub',
+                    text: t(ResourceKeys.breadcrumb.hub, {hubName: this.getShortHubName(hubName)})
+                });
+                return;
             }
-        }
-        frags.forEach((frag, index) => {
-            if ('digitalTwins' === frag) {
-                const interfaceFrag = this.getInterfaceFrag(search);
-                if (interfaceFrag) {
-                    items.push(interfaceFrag);
+
+            if (pathComponent.toLowerCase() === ROUTE_PARTS.DEVICES.toLowerCase()) {
+                items.push({
+                    href: (index !== pathComponents.length - 1) ? `#/${ROUTE_PARTS.RESOURCE}/${hubName}/${ROUTE_PARTS.DEVICES}` : '',
+                    key: 'Devices',
+                    text: t(ResourceKeys.breadcrumb.devices)
+                });
+                return;
+            }
+
+            if (pathComponent.toLowerCase() === ROUTE_PARTS.DETAIL.toLowerCase()) {
+                const deviceId = this.getDeviceIdFromSearch(search);
+                items.push({
+                    href: `#/${ROUTE_PARTS.RESOURCE}/${hubName}/${ROUTE_PARTS.DEVICES}/${ROUTE_PARTS.DETAIL}/${ROUTE_PARTS.IDENTITY}?${ROUTE_PARAMS.DEVICE_ID}=${deviceId}`,
+                    key: 'Device',
+                    text: deviceId
+                });
+
+                if (index < pathComponents.length - 1 && pathComponents[index + 1].toLowerCase() !== ROUTE_PARTS.DIGITAL_TWINS.toLowerCase()) {
+                    const remainingPathComponents = pathComponents.slice(index + 1);
+                    items.push(...this.buildDeviceDetails(remainingPathComponents, hubName, deviceId, t));
                 }
-            } else {
-                if (index < frags.length - 1) {
-                    // tslint:disable-next-line: no-any
-                    items.push({ text: t((ResourceKeys.deviceContent.navBar as any)[frag]), key: frag, href: frag });
-                } else {
-                    // tslint:disable-next-line: no-any
-                    items.push({ text: t((ResourceKeys.deviceContent.navBar as any)[frag]), key: frag });
+                return;
+            }
+
+            if (pathComponent.toLowerCase() === ROUTE_PARTS.DIGITAL_TWINS.toLowerCase()) {
+                const deviceId = this.getDeviceIdFromSearch(search);
+                const interfaceId = this.getInterfaceIdFromSearch(search);
+                items.push({
+                    href: `#/${ROUTE_PARTS.DEVICES}/${ROUTE_PARTS.DETAIL}/${ROUTE_PARTS.DIGITAL_TWINS}/${ROUTE_PARTS.INTERFACES}/?${ROUTE_PARAMS.DEVICE_ID}=${deviceId}&${ROUTE_PARAMS.INTERFACE_ID}=${interfaceId}`,
+                    key: `device_${deviceId}_${interfaceId}`,
+                    text: interfaceId
+                });
+
+                if (index < pathComponents.length - 1) {
+                    const remainingPathComponents = pathComponents.slice(index + 1);
+                    items.push(...this.buildDigitalTwinDetails(remainingPathComponents, hubName, deviceId, interfaceId, t));
                 }
+                return;
             }
         });
+
         return items;
+    }
+
+    private buildDeviceDetails = (pathComponents: string[], hubName: string, deviceId: string, t: TranslationFunction): BreadcrumbEntry[] => {
+        const entries: BreadcrumbEntry[] = [];
+        const paths = [...pathComponents];
+        const query = `${ROUTE_PARAMS.DEVICE_ID}=${deviceId}`;
+        let basePath = `#/${ROUTE_PARTS.RESOURCE}/${hubName}/${ROUTE_PARTS.DEVICES}/${ROUTE_PARTS.DETAIL}`;
+        while (paths.length > 0) {
+            basePath = `${basePath}/${paths[0]}`;
+            entries.push({
+                href: paths.length !== 1 ? `${basePath}${query}` : '',
+                key: paths[0],
+                text: t((ResourceKeys.deviceContent.navBar as any)[paths[0]]) // tslint:disable-line:no-any
+            });
+
+            paths.splice(0, 1);
+        }
+
+        return entries;
+    }
+
+    private buildDigitalTwinDetails = (pathComponents: string[], hubName: string, deviceId: string, interfaceId: string, t: TranslationFunction): BreadcrumbEntry[] => {
+        const entries: BreadcrumbEntry[] = [];
+        const paths = [...pathComponents];
+        const query = `${ROUTE_PARAMS.DEVICE_ID}=${deviceId}&${ROUTE_PARAMS.INTERFACE_ID}=${interfaceId}`;
+        let basePath = `#/${ROUTE_PARTS.RESOURCE}/${hubName}/${ROUTE_PARTS.DEVICES}/${ROUTE_PARTS.DETAIL}/${ROUTE_PARTS.DIGITAL_TWINS}`;
+        while (paths.length > 0) {
+            basePath = `${basePath}/${paths[0]}`;
+            entries.push({
+                href: paths.length !== 1 ? `${basePath}${query}` : '',
+                key: paths[0],
+                text: t((ResourceKeys.deviceContent.navBar as any)[paths[0]]) // tslint:disable-line:no-any
+            });
+
+            paths.splice(0, 1);
+        }
+
+        return entries;
+    }
+
+    private readonly getShortHubName = (hostName: string) => {
+        return hostName && hostName.replace(/\..*/, '');
     }
 
     private readonly getDeviceIdFromSearch = (search: string) => {
         return new URLSearchParams(search).get(ROUTE_PARAMS.DEVICE_ID);
     }
 
-    private readonly getDeviceFrag = (search: string) => {
-        const deviceId = this.getDeviceIdFromSearch(search);
-        if (deviceId) {
-            return {
-                href: `#/${ROUTE_PARTS.DEVICES}/${ROUTE_PARTS.DETAIL}/${ROUTE_PARTS.IDENTITY}/?${ROUTE_PARAMS.DEVICE_ID}=${deviceId}`,
-                key: `device_${deviceId}`,
-                text: deviceId
-            };
-        }
-        return null;
-    }
-
-    public readonly getInterfaceFrag = (search: string) => {
-        const deviceId = this.getDeviceIdFromSearch(search);
-        // can't have a device interface without a device
-        if (deviceId) {
-            const interfaceId = new URLSearchParams(search).get(ROUTE_PARAMS.INTERFACE_ID);
-            if (interfaceId && '' !== interfaceId) {
-                return {
-                    href: `#/${ROUTE_PARTS.DEVICES}/${ROUTE_PARTS.DETAIL}/${ROUTE_PARTS.DIGITAL_TWINS}/${ROUTE_PARTS.INTERFACES}/?${ROUTE_PARAMS.DEVICE_ID}=${deviceId}&${ROUTE_PARAMS.INTERFACE_ID}=${interfaceId}`,
-                    key: `device_${deviceId}_${interfaceId}`,
-                    text: interfaceId
-                };
-            }
-        }
-        return null;
+    private readonly getInterfaceIdFromSearch = (search: string) => {
+        return new URLSearchParams(search).get(ROUTE_PARAMS.INTERFACE_ID);
     }
 }
