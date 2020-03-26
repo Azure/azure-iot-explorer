5f1b200df753dd16349c03bad258efd19d70aae9
ying xue <yingxue@microsoft.com>
2020-03-26 10:13:22 -0700

    electron only for local files

--- src/app/settings/state.ts
+++ src/app/settings/state.ts
@@ -7,6 +7,7 @@ import { IM } from '../shared/types/types';
 import { REPOSITORY_LOCATION_TYPE } from '../constants/repositoryLocationTypes';
 import { PRIVATE_REPO_CONNECTION_STRING_NAME, REPO_LOCATIONS, LOCAL_FILE_EXPLORER_PATH_NAME } from '../constants/browserStorage';
 import { MILLISECONDS_IN_MINUTE, PUBLIC_REPO_HOSTNAME } from '../constants/shared';
+import { appConfig, HostMode } from '../../appConfig/appConfig';
 export const OFFSET_IN_MINUTES = 15;
 
 export interface RepositoryLocationSettings {
@@ -36,7 +37,7 @@ export interface LocalFolderSettings {
     path: string;
 }
 
-export const applicationStateInitial =
+export const applicationStateInitial = appConfig.hostMode === HostMode.Electron ?
     Record<ApplicationStateInterface>({
         localFolderSettings: {
             path: localStorage.getItem(LOCAL_FILE_EXPLORER_PATH_NAME) || '',
@@ -51,6 +52,21 @@ export const applicationStateInitial =
         },
         repositoryLocations: localStorage.getItem(REPO_LOCATIONS) ?
             localStorage.getItem(REPO_LOCATIONS).split(',').map(location => location as REPOSITORY_LOCATION_TYPE) :
+            [REPOSITORY_LOCATION_TYPE.Public, REPOSITORY_LOCATION_TYPE.Device],
+        showSettings: false
+    }) :
+    Record<ApplicationStateInterface>({
+        localFolderSettings: null,
+        privateRepositorySettings: {
+            privateConnectionString: localStorage.getItem(PRIVATE_REPO_CONNECTION_STRING_NAME) || '',
+            privateRepoTimestamp: new Date().getTime() - (OFFSET_IN_MINUTES * MILLISECONDS_IN_MINUTE),
+            privateRepoToken: ''
+        },
+        publicRepositorySettings: {
+            publicRepoHostName: PUBLIC_REPO_HOSTNAME
+        },
+        repositoryLocations: localStorage.getItem(REPO_LOCATIONS) ?
+            localStorage.getItem(REPO_LOCATIONS).split(',').map(location => location as REPOSITORY_LOCATION_TYPE).filter(location => location !== REPOSITORY_LOCATION_TYPE.Local) :
             [REPOSITORY_LOCATION_TYPE.Public, REPOSITORY_LOCATION_TYPE.Device],
         showSettings: false
     });
