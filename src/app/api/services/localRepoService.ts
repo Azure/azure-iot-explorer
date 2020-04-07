cbaf756a0cab227e0d805f3baf36a2835289cbb3
YingXue <yingxue@microsoft.com>
2020-04-07 14:07:25 -0700

    use @id to match dtdl not names (#250)
    * use @id to match dtdl not names
    
    * update strings

--- src/app/api/services/localRepoService.ts
+++ src/app/api/services/localRepoService.ts
@@ -4,9 +4,9 @@
  **********************************************************/
 import { READ_FILE, CONTROLLER_API_ENDPOINT, DataPlaneStatusCode } from './../../constants/apiConstants';
 
-export const fetchLocalFile = async (path: string): Promise<string> => {
+export const fetchLocalFile = async (path: string, fileName: string): Promise<string> => {
     try {
-        const response = await fetch(`${CONTROLLER_API_ENDPOINT}${READ_FILE}/${encodeURIComponent(path)}`);
+        const response = await fetch(`${CONTROLLER_API_ENDPOINT}${READ_FILE}/${encodeURIComponent(path)}/${encodeURIComponent(fileName)}`);
         if (await response.status === DataPlaneStatusCode.NotFound) {
             throw new Error();
         }
