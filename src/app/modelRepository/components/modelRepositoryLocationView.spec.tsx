d076fe16fd468c58edda493edc53105a05b4fbd4
Rick Kessler <rkessler@microsoft.com>
2020-05-01 13:52:05 -0700

    temp.

---
+++ src/app/modelRepository/components/modelRepositoryLocationView.spec.tsx
@@ -0,0 +1,16 @@
+/***********************************************************
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License
+ **********************************************************/
+import * as React from 'react';
+import { ModelRepositoryLocationView } from './modelRepositoryLocationView';
+
+describe('ModelRepositoryLocationView', () => {
+    it('matches snapshot when empty settings array passed provided', () => {
+        expect(true).toEqual(false);
+    });
+
+    it('matches snapshot when settings array provided', () => {
+        expect(true).toEqual(false);
+    });
+});
