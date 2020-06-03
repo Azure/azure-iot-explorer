59d9d7b588a5c84fa8c182940a7e169bc82e61f7
asrudra <33266774+asrudra@users.noreply.github.com>
2020-06-03 15:12:59 -0700

    reduxless device list (#293)

--- src/app/devices/deviceList/saga.ts
+++ src/app/devices/deviceList/saga.ts
@@ -2,12 +2,14 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License
  **********************************************************/
-import { takeEvery, takeLatest } from 'redux-saga/effects';
+import { takeEvery, takeLatest, all } from 'redux-saga/effects';
 import { listDevicesSaga } from './sagas/listDeviceSaga';
 import { deleteDevicesSaga } from './sagas/deleteDeviceSaga';
 import { listDevicesAction, deleteDevicesAction } from './actions';
 
-export default [
-    takeLatest(listDevicesAction.started.type, listDevicesSaga),
-    takeEvery(deleteDevicesAction.started.type, deleteDevicesSaga)
-];
+export function* deviceListSaga() {
+    yield all([
+        takeLatest(listDevicesAction.started.type, listDevicesSaga),
+        takeEvery(deleteDevicesAction.started.type, deleteDevicesSaga)
+    ]);
+}
