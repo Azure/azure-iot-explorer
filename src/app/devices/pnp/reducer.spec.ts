a9bfb1b79ab29ed8c6a049792fecbe27db43d52a
unknown <kalian1127@gmail.com>
2021-03-09 23:38:28 -0800

    update tests

--- src/app/devices/pnp/reducer.spec.ts
+++ src/app/devices/pnp/reducer.spec.ts
@@ -5,14 +5,17 @@
 import 'jest';
 import {
     FETCH_MODEL_DEFINITION,
+    GET_MODULE_IDENTITY_TWIN,
     GET_TWIN,
+    UPDATE_MODULE_IDENTITY_TWIN,
     UPDATE_TWIN
   } from '../../constants/actionTypes';
 import {
     getModelDefinitionAction,
     getDeviceTwinAction,
     updateDeviceTwinAction,
-    GetModelDefinitionActionParameters
+    GetModelDefinitionActionParameters,
+    getModuleTwinAction
 } from './actions';
 import { pnpReducer } from './reducer';
 import { pnpStateInitial  } from './state';
@@ -149,6 +152,64 @@ describe('pnpReducer', () => {
 
         it (`handles ${UPDATE_TWIN}/ACTION_FAILED action`, () => {
             const action = updateDeviceTwinAction.failed({error: -1, params: deviceTwin});
+            expect(pnpReducer(initialState, action).twin.synchronizationStatus).toEqual(SynchronizationStatus.failed);
+        });
+    });
+
+    describe('module twin scenarios', () => {
+
+        /* tslint:disable */
+        const moduleTwin: any = {
+            "deviceId": "testDevice",
+            "moduleId": "testModule",
+            "modelId": "urn:azureiot:samplemodel;1",
+            "properties" : {
+                desired: {
+                    environmentalSensor: {
+                        state: true,
+                        __t: 'c'
+                    }
+                }
+            }
+        };
+        /* tslint:enable */
+
+        it (`handles ${GET_MODULE_IDENTITY_TWIN}/ACTION_START action`, () => {
+            const action = getModuleTwinAction.started({deviceId: moduleTwin.deviceId, moduleId: moduleTwin.moduleId});
+            expect(pnpReducer(pnpStateInitial(), action).twin.synchronizationStatus).toEqual(SynchronizationStatus.working);
+        });
+
+        it (`handles ${GET_MODULE_IDENTITY_TWIN}/ACTION_DONE action`, () => {
+            const action = getModuleTwinAction.done({params: {deviceId: moduleTwin.deviceId, moduleId: moduleTwin.moduleId}, result: moduleTwin});
+            expect(pnpReducer(pnpStateInitial(), action).twin.payload).toEqual(moduleTwin);
+        });
+
+        it (`handles ${GET_MODULE_IDENTITY_TWIN}/ACTION_FAILED action`, () => {
+            const action = getModuleTwinAction.failed({error: -1, params: {deviceId: moduleTwin.deviceId, moduleId: moduleTwin.moduleId}});
+            expect(pnpReducer(pnpStateInitial(), action).twin.synchronizationStatus).toEqual(SynchronizationStatus.failed);
+        });
+
+        let initialState = pnpStateInitial();
+        initialState = initialState.merge({
+            twin: {
+                payload: moduleTwin as any,
+                synchronizationStatus: SynchronizationStatus.fetched
+            }
+        });
+        moduleTwin.properties.desired.environmentalSensor.state = false;
+
+        it (`handles ${UPDATE_MODULE_IDENTITY_TWIN}/ACTION_START action`, () => {
+            const action = updateDeviceTwinAction.started(moduleTwin);
+            expect(pnpReducer(initialState, action).twin.synchronizationStatus).toEqual(SynchronizationStatus.updating);
+        });
+
+        it (`handles ${UPDATE_MODULE_IDENTITY_TWIN}/ACTION_DONE action`, () => {
+            const action = updateDeviceTwinAction.done({params: moduleTwin, result: moduleTwin});
+            expect(pnpReducer(initialState, action).twin.payload).toEqual(moduleTwin);
+        });
+
+        it (`handles ${UPDATE_MODULE_IDENTITY_TWIN}/ACTION_FAILED action`, () => {
+            const action = updateDeviceTwinAction.failed({error: -1, params: moduleTwin});
             expect(pnpReducer(initialState, action).twin.synchronizationStatus).toEqual(SynchronizationStatus.failed);
         });
     });
