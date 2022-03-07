0d50645d7f1481188c98b4637b2ad79347325a5b
Ryan K <ryan.k@outlook.com>
2022-03-07 11:20:03 -0800

    Updated some plugins and working towards webpack v4 -> v5

--- webpack.dev.ts
+++ webpack.dev.ts
@@ -16,8 +16,8 @@ const config: webpack.Configuration = merge(common, {
 
     module: {
         rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: 'awesome-typescript-loader', exclude: /node_modules/ },
+            // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
+            { test: /\.tsx?$/, loader: 'ts-loader', exclude: /node_modules/ },
 
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
             { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader', exclude: /node_modules/ },
--- webpack.electron.ts
+++ webpack.electron.ts
@@ -16,8 +16,8 @@ const config: webpack.Configuration = merge(common, {
 
     module: {
         rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
+            // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
+            { test: /\.tsx?$/, loader: 'ts-loader' },
             {
                 test: /\.(scss|css)$/,
                 use: [
