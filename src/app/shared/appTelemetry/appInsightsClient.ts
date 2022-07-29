0c00d75bdff99d226cdd45392be3216277fb9cda
manasi <t-shahmanasi@microsoft.com>
2022-07-29 12:47:53 -0700

    verification code for testing purposes

--- src/app/shared/appTelemetry/appInsightsClient.ts
+++ src/app/shared/appTelemetry/appInsightsClient.ts
@@ -1,6 +1,7 @@
 import { ApplicationInsights } from '@microsoft/applicationinsights-web';
 import { TELEMETRY_EVENTS } from '../../constants/telemetry';
 import { appConfig } from '../../../appConfig/appConfig';
+
 export class AppInsightsClient {
     private static instance: ApplicationInsights;
 
@@ -12,6 +13,8 @@ export class AppInsightsClient {
             } });
             try {
                 appInsights.loadAppInsights();
+                // TODO: test on domain joined computer
+                console.log('INTERNAL: ' + verifyMicrosoftInternalDomain());
                 appInsights.trackEvent({name: TELEMETRY_EVENTS.INIT});
                 AppInsightsClient.instance = appInsights;
             } catch (e) {
@@ -23,3 +26,15 @@ export class AppInsightsClient {
         return AppInsightsClient.instance;
     }
 }
+
+// TODO: move to a better place
+const verifyMicrosoftInternalDomain = (): boolean => {
+    const msftInternalDomains = ['redmond.corp.microsoft.com', 'northamerica.corp.microsoft.com', 'fareast.corp.microsoft.com', 'ntdev.corp.microsoft.com', 'wingroup.corp.microsoft.com', 'southpacific.corp.microsoft.com', 'wingroup.windeploy.ntdev.microsoft.com', 'ddnet.microsoft.com', 'europe.corp.microsoft.com'];
+    const userDnsDomain = process.env.USERDNSDOMAIN;
+    if (!userDnsDomain) {
+        return false;
+    }
+
+    const domain = userDnsDomain.toLowerCase();
+    return msftInternalDomains.some(msftDomain => domain === msftDomain);
+};
