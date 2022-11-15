d5c0d55aba95e99e83e69315bc5c99c5ded20bf6
Elsie4ever <3467996@gmail.com>
2022-11-15 12:58:09 -0800

    update folder

---
+++ src/app/devices/pnp/components/modelConfiguration/command.spec.tsx
@@ -0,0 +1,30 @@
+/***********************************************************
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License
+ **********************************************************/
+ import 'jest';
+ import * as React from 'react';
+ import { shallow } from 'enzyme';
+ import { Command } from './command';
+ import * as pnpStateContext from '../../../../shared/contexts/pnpStateContext';
+ import { PnpStateInterface, pnpStateInitial } from '../../state';
+ import { SynchronizationStatus } from '../../../../api/models/synchronizationStatus';
+ 
+ const search = '?id=device1&componentName=foo&interfaceId=urn:iotInterfaces:com:interface1;1';
+ const pathname = `/#/devices/deviceDetail/ioTPlugAndPlay/${search}`;
+ jest.mock('react-router-dom', () => ({
+     useLocation: () => ({ search, pathname })
+ }));
+ 
+ describe('Command', () => {
+     it('matches snapshot', () => {
+         const initialState: PnpStateInterface = pnpStateInitial().merge({
+             twin: {
+                 synchronizationStatus: SynchronizationStatus.working
+             }
+         });
+         jest.spyOn(pnpStateContext, 'usePnpStateContext').mockReturnValue({ pnpState: initialState, dispatch: jest.fn()});
+         expect(shallow(<Command/>)).toMatchSnapshot();
+     });
+ });
+ 
